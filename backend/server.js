f1b6ecfe1a7f3abe968b28b9a3c42e3a8301ee4b
Shankhya Subhra Chatterjee <shankhyasubhrac@outlook.com>
2025-03-03 00:02:33 +0530

    Deplotment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import path from 'path';
 import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -8,7 +10,9 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 import { app, server } from "./socket/socket.js";
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,10 +23,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req,res) => {
-//     //route route http://localhost:5000/
-//     res.send("Lets go")
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () => {
