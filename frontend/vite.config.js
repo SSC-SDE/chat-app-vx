ad9ecd1b2c8091ada310f5c6aaf6477ac0f3a6aa
Shankhya Subhra Chatterjee <shankhyasubhrac@outlook.com>
2025-03-03 00:25:52 +0530

    updating render provided deployment host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://chat-app-prod-0tz8.onrender.com/",
       }
     }
   }
