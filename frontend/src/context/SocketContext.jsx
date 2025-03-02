202b9fa10381a1f137a0d53483c4f74ba0011539
Shankhya Subhra Chatterjee <shankhyasubhrac@outlook.com>
2025-03-03 00:31:47 +0530

    socket update missed for one file

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-prod-0tz8.onrender.com/login", {
+      const socket = io("https://chat-app-prod-0tz8.onrender.com/", {
         query: {
           userId: authUser._id,
         },
